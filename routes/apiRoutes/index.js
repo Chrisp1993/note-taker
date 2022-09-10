949811d599c6d1939c5a1987a3bb7ec2af383c91
Chrisp1993 <chrispollio93@gmail.com>
2022-09-10 19:34:24 -0400

    added the api routes and the html routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { notes } = require('../../db/db.json');
+const {createNewNote, deleteNoteData} = require('../../lib/notes.js');
+const uniqid = require('uniqid');
+
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+
+router.post('/notes', (req, res) => {
+    req.body.id = uniqid();
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+});
+
+
+
+// Manage delete requests
+router.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedNotes = deleteNoteData(id, notes);
+
+    res.send('delete request received');
+});
+
+module.exports = router;
