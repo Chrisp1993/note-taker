a8d4a5bebebda558bbb5c668a6e1cdfc70c86e2e
Chrisp1993 <chrispollio93@gmail.com>
2022-09-10 16:23:13 -0400

    added the ability to retrieve the notes from the database and render them in the sidebar of the app

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -181,3 +181,19 @@ const handleNoteDelete = (e) => {
       noteListItems.forEach((note) => noteList[0].append(note));
     }
   };
+
+
+  // Retrieve the notes from database and render them in the sidebar
+const getAndRenderNotes = () => getNotes().then(renderNoteList);
+
+if (window.location.pathname === '/notes') {
+  saveNoteBtn.addEventListener('click', handleNoteSave);
+
+  newNoteBtn.addEventListener('click', handleNewNoteView);
+
+  noteTitle.addEventListener('keyup', handleRenderSaveBtn);
+
+  noteText.addEventListener('keyup', handleRenderSaveBtn);
+}
+
+getAndRenderNotes();
