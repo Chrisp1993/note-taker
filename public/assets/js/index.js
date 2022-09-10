01441d533022b32382db9b93b2eab14978271d4e
Chrisp1993 <chrispollio93@gmail.com>
2022-09-10 16:19:26 -0400

    Added the delete a note feature which will render the list without the note when the delete button has been clicked. It will also render the html element with or without a delete button and set the active note to an empty object so the user can add a new note

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -63,7 +63,7 @@ const renderActiveNote = () => {
 
     noteTitle.value = activeNote.title;
     noteText.value = activeNote.text;
-    
+
   } else {
     noteTitle.removeAttribute('readonly');
     noteText.removeAttribute('readonly');
@@ -83,3 +83,101 @@ const handleNoteSave = () => {
     renderActiveNote();
   });
 };
+
+
+// Delete the note that has been selected
+const handleNoteDelete = (e) => {
+    // This will prevent the click listener for the list from being called when the button inside of it has been clicked
+    e.stopPropagation();
+  
+    const note = e.target;
+    const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
+  
+    if (activeNote.id === noteId) {
+      activeNote = {};
+    }
+  
+    deleteNote(noteId).then(() => {
+      getAndRenderNotes();
+      renderActiveNote();
+    });
+  };
+  
+
+  // Set and display the activeNote
+  const handleNoteView = (e) => {
+    e.preventDefault();
+    activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
+    renderActiveNote();
+  };
+  
+
+  // Set activeNote to an empty element and allow the user to enter new note
+  const handleNewNoteView = (e) => {
+    activeNote = {};
+    renderActiveNote();
+  };
+  
+  const handleRenderSaveBtn = () => {
+    if (!noteTitle.value.trim() || !noteText.value.trim()) {
+      hide(saveNoteBtn);
+
+    } else {
+      show(saveNoteBtn);
+    }
+  };
+  
+  // Render a list of note titles
+  const renderNoteList = async (notes) => {
+    let jsonNotes = await notes.json();
+
+    if (window.location.pathname === '/notes') {
+      noteList.forEach((el) => (el.innerHTML = ''));
+    }
+  
+    let noteListItems = [];
+  
+    // Return the HTML element w/without a delete button included
+    const createLi = (text, delBtn = true) => {
+      const liEl = document.createElement('li');
+      liEl.classList.add('list-group-item');
+  
+      const spanEl = document.createElement('span');
+      spanEl.classList.add('list-item-title');
+      spanEl.innerText = text;
+      spanEl.addEventListener('click', handleNoteView);
+  
+      liEl.append(spanEl);
+  
+      if (delBtn) {
+        const delBtnEl = document.createElement('i');
+        delBtnEl.classList.add(
+          'fas',
+          'fa-trash-alt',
+          'float-right',
+          'text-danger',
+          'delete-note'
+        );
+        delBtnEl.addEventListener('click', handleNoteDelete);
+  
+        liEl.append(delBtnEl);
+      }
+  
+      return liEl;
+    };
+  
+    if (jsonNotes.length === 0) {
+      noteListItems.push(createLi('No saved Notes', false));
+    }
+  
+    jsonNotes.forEach((note) => {
+      const li = createLi(note.title);
+      li.dataset.note = JSON.stringify(note);
+  
+      noteListItems.push(li);
+    });
+  
+    if (window.location.pathname === '/notes') {
+      noteListItems.forEach((note) => noteList[0].append(note));
+    }
+  };
