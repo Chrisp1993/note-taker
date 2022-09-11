e39af85c0a71380a561d0fc2abe6918cad0aaca5
Chrisp1993 <chrispollio93@gmail.com>
2022-09-10 20:00:30 -0400

    added the lib with the note.js for create new note and delete note data, the db.json and the notes test

---
+++ __tests__/notes.test.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const { createNewNote, deleteNoteData } = require('../lib/notes');
+const { notes } = require('../db/db.json');
+jest.mock('fs');
+
+
+test('create new note object', () => {
+    const newNote = createNewNote(
+        { title: 'Test Title', text: 'test note text'},
+        notes
+        );
+
+    expect(newNote.title).toBe('Test Title');
+    
+    expect(newNote.text).toBe('test note text');
+});
+
+
+test('delete a note object', () => {
+    const noteArr = [
+        {
+            title: 'Test 1',
+            text: 'test 1 text',
+            id: '1'
+        },
+        {
+            title: 'test 2',
+            text: 'test 2 text',
+            id: '2'
+        },
+    ];
+    const updatedNoteArr = deleteNoteData(1, noteArr);
+
+    expect(updatedNoteArr.length).toEqual(1)
+})
